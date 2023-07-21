f4cab293a8f56e17938b3608f9b52e25bfee6206
Edgar Allen <51770818+landerpogiii3@users.noreply.github.com>
2023-07-21 12:43:17 +0800

    Create SysAd Firewall rules.sh
    added the SysAd Firewall rules.sh

---
+++ SysAd Firewall rules.sh
@@ -0,0 +1,15 @@
+sudo ufw allow from 10.10.5.35 to any port 2222
+sudo ufw allow from 10.10.5.39 to any port 2222
+sudo ufw allow from 10.10.5.40 to any port 2222
+sudo ufw allow from 10.10.5.35 to any port 9090
+sudo ufw allow from 10.10.5.39 to any port 9090
+sudo ufw allow from 10.10.5.40 to any port 9090
+sudo ufw allow from 10.10.5.35 to any port 8443
+sudo ufw allow from 10.10.5.39 to any port 8443
+sudo ufw allow from 10.10.5.40 to any port 8443
+sudo ufw allow 3478/udp
+sudo ufw allow 8080
+sudo ufw allow 8880
+sudo ufw allow 8843
+sudo ufw allow 80
+sudo ufw allow 443
