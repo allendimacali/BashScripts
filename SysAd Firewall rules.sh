c87c1421e2260b9f0ccea9ccb81a7bd57679b8cf
Edgar Allen <51770818+landerpogiii3@users.noreply.github.com>
2023-08-11 15:18:32 +0800

    Update SysAd Firewall rules.sh
    Added RDP ports

--- SysAd Firewall rules.sh
+++ SysAd Firewall rules.sh
@@ -7,9 +7,9 @@ sudo ufw allow from 10.10.5.40 to any port 9090
 sudo ufw allow from 10.10.5.36 to any port 8443
 sudo ufw allow from 10.10.5.39 to any port 8443
 sudo ufw allow from 10.10.5.40 to any port 8443
-sudo ufw allow from 10.10.5.36 to any port 5001
-sudo ufw allow from 10.10.5.39 to any port 5001
-sudo ufw allow from 10.10.5.40 to any port 5001
+sudo ufw allow from 10.10.5.36 to any port 3389
+sudo ufw allow from 10.10.5.39 to any port 3389
+sudo ufw allow from 10.10.5.40 to any port 3389
 sudo ufw allow 3478/udp
 sudo ufw allow 8080
 sudo ufw allow 8880
