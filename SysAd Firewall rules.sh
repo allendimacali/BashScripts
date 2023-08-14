a68c58d035c197888986c9551fdb4dfe37b9dc2b
Edgar Allen <51770818+landerpogiii3@users.noreply.github.com>
2023-08-14 13:34:51 +0800

    Update SysAd Firewall rules.sh
    Removed gateway IP

--- SysAd Firewall rules.sh
+++ SysAd Firewall rules.sh
@@ -1,19 +1,15 @@
-sudo ufw allow from 10.10.5.36 to any port 22
-sudo ufw allow from 10.10.5.39 to any port 22
-sudo ufw allow from 10.10.5.40 to any port 22
-sudo ufw allow from 10.10.70.1 to any port 22
+sudo ufw allow from 10.10.5.36 to any port 2222
+sudo ufw allow from 10.10.5.39 to any port 2222
+sudo ufw allow from 10.10.5.40 to any port 2222
 sudo ufw allow from 10.10.5.36 to any port 9090
 sudo ufw allow from 10.10.5.39 to any port 9090
 sudo ufw allow from 10.10.5.40 to any port 9090
-sudo ufw allow from 10.10.70.1 to any port 9090
 sudo ufw allow from 10.10.5.36 to any port 8443
 sudo ufw allow from 10.10.5.39 to any port 8443
 sudo ufw allow from 10.10.5.40 to any port 8443
-sudo ufw allow from 10.10.70.1 to any port 8443
 sudo ufw allow from 10.10.5.36 to any port 3389
 sudo ufw allow from 10.10.5.39 to any port 3389
 sudo ufw allow from 10.10.5.40 to any port 3389
-sudo ufw allow from 10.10.70.1 to any port 3389
 sudo ufw allow 3478/udp
 sudo ufw allow 8080
 sudo ufw allow 8880
