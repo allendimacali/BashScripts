7c57b36c37400daa93e7cf42d3352aeb8b2804e7
Edgar Allen <51770818+allendimacali@users.noreply.github.com>
2025-04-24 09:41:28 +0800

    Update SysAd Firewall rules.sh

--- SysAd Firewall rules.sh
+++ SysAd Firewall rules.sh
@@ -11,6 +11,7 @@ sudo ufw allow from 10.10.5.36 to any port 3389
 sudo ufw allow from 10.10.5.39 to any port 3389
 sudo ufw allow from 10.10.5.40 to any port 3389
 sudo ufw allow from 10.10.5.40 to any port 9130 proto tcp
+sudo ufw allow from 10.10.4.95 to any port 9130 proto tcp
 sudo ufw allow 3478/udp
 sudo ufw allow 8080
 sudo ufw allow 8880
